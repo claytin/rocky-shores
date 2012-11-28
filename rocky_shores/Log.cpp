914780bbbf9ccbc29970f68744dedf664141394a
mgc480 <masonclayton@live.com>
2012-11-27 23:52:24 -0800

    (quick fix) forgot parentheses for funciton 'c_str()'

--- rocky_shores/Log.cpp
+++ rocky_shores/Log.cpp
@@ -9,15 +9,15 @@ Log::~Log(void){
 }
 
 void Log::status(std::string message, int line, const char * file){
-	std::cout << "::status: " <<message.c_str;	//print out message
+	std::cout << "::status: " << message.c_str();	//print out message
 	if(line != -1 && file != 0){	//print out message
 		std::cout << " @ " << file << ":" << line;
 	}
 	std::cout << std::endl;	//end line
 }
 
-void Log::error(std::string message, int line = -1, const char * file){
-	std::cout << "::status: " <<message.c_str;	//print out message
+void Log::error(std::string message, int line, const char * file){
+	std::cout << "::status: " << message.c_str();	//print out message
 	if(line != -1 && file != 0){	//print out message
 		std::cout << " @ " << file << ":" << line;
 	}
