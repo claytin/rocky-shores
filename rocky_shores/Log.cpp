43551f59c7e511f242ac08e15d0dab7bb7aaa65a
mgc480 <masonclayton@live.com>
2012-11-27 21:04:49 -0800

    adding basic log class

---
+++ rocky_shores/Log.cpp
@@ -0,0 +1,27 @@
+#include "Log.h"
+
+Log::Log(void){
+
+}
+
+Log::~Log(void){
+
+}
+
+void Log::status(std::string message, int line, const char * file){
+	std::cout << "::status: " <<message.c_str;	//print out message
+	if(line != -1 && file != 0){	//print out message
+		std::cout << " @ " << file << ":" << line;
+	}
+	std::cout << std::endl;	//end line
+}
+
+void Log::error(std::string message, int line = -1, const char * file){
+	std::cout << "::status: " <<message.c_str;	//print out message
+	if(line != -1 && file != 0){	//print out message
+		std::cout << " @ " << file << ":" << line;
+	}
+	std::cout << std::endl;	//end line
+
+	//TODO coming soon: open a dialogue
+}
---
+++ rocky_shores/Log.h
@@ -0,0 +1,18 @@
+//Log.h - used as a global comunication system to log file or console
+//very simple takes arguments and prints them to console or a file (whichever is selected)
+//-mgc480
+
+#pragma once
+#include <iostream>	//uses iostream for console output
+#include <stireg.h>	//uses stirng for message
+
+class Log{
+
+public:
+	Log(void);
+	~Log(void);
+
+	void status(std::string message, int line = -1, const char * file = 0);	//prints the message to the console
+	void error(std::string message, int line = -1, const char * file = 0);	//same as status but also brings up dialogue
+};
+
