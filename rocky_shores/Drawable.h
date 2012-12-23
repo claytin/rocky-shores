5ec6a2f47d09e63229959fa67800a5c994df6f5b
mgc480 <masonclayton@live.com>
2012-12-22 16:22:07 -0800

    adding drawable class as a base object that can be rendered

---
+++ rocky_shores/Drawable.h
@@ -0,0 +1,17 @@
+//Drawable.h / Drawable.cpp
+//base class for any sprite or object that can be rendered
+//has position, a mesh, and optionaly texture or color
+
+#pragma once
+#include <glm\glm.hpp>
+
+class Drawable{
+
+public:
+	Drawable(void);
+	~Drawable(void);
+
+private:
+	//glm::mat4 cords;
+};
+
