d723b7933d285e2d31c5db48a32095077f5a8cab
mason <mason@pisscake.com>
2014-10-08 10:51:48 -0700

    config now has versioning. yay?

--- config.h
+++ config.h
@@ -1,2 +1,12 @@
+//kinda sorta follows the semantic versioning spec
+//string should be in format:
+//VERSION_MAJOR.VERSION_MINOR.VERSION_PATCH-VERSION_NAME+VERSION_BUILD
+#define VERSION_STRING "0.0.0-dev+1"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_NAME "dev"
+#define VERSION_BUILD 1
+
 #define INITIAL_RESOURCE_PATH "res/initial.cfg"
 #define PLUGIN_PATH "res/plugins.cfg"
